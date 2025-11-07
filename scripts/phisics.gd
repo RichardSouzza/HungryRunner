b79fb8fbbab20d15fb718c576528bf300c96dc0d
Richard Souza <souzza.richard25@gmail.com>
2025-11-07 20:21:57 -0300

    feat: Add background

--- scripts/phisics.gd
+++ scripts/phisics.gd
@@ -4,14 +4,13 @@ extends CharacterBody2D
 
 var RUN_SPEED = 400
 var JUMP_SPEED = -800
-
-var gravity = 2600
+var GRAVITY = 2600
 
 func _physics_process(delta):
     move_and_slide()
     if not is_on_floor():
         animation.play("jump")
-        velocity.y += gravity * delta
+        velocity.y += GRAVITY * delta
     else:
         animation.play("run")
 
@@ -22,7 +21,6 @@ func get_input():
 
     if Input.is_action_just_pressed("jump"):
         if is_on_floor():
-            print("Jumping!")
             velocity.y = JUMP_SPEED
 
     velocity.x = RUN_SPEED
